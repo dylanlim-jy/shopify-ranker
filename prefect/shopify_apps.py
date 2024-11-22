e8c422f922e33b0dedfe37c70e42bca0ee69aa46
Dylan Lim <dylan@Dylans-MacBook-Air-M1.local>
2024-11-22 13:44:21 +0800

    Restructure filesystem

--- prefect/shopify_apps.py
+++ prefect/shopify_apps.py
@@ -2,7 +2,8 @@ import re
 import random
 import hashlib
 from playwright.sync_api import sync_playwright, expect
-from prefect import task, get_run_logger
+from prefect import task, flow, get_run_logger
+from prefect_sqlalchemy import SqlAlchemyConnector
 
 @task(retries=2, retry_delay_seconds=60)
 def test_shopify_app_page(url: str) -> bool:
@@ -106,3 +107,88 @@ def extract_shopify_apps(url: str) -> list:
         logger.warning("Function test_shopify_app_page failed.")
         return
 
+
+@task(retries=2, retry_delay_seconds=30)
+def compare_sha_keys(source_data: list[dict]) -> list[dict]:
+    """Takes the sha256_surrogate_key field from source data and uses it to query
+    the sink; returns records that have no matches with the sink data.
+    
+    Args:
+        source_data (list[dict]): List of dictionaries from the Shopify app scraper
+    
+    Returns:
+        records_to_write (list[dict]): Filtered list of dictionaries from Shopify app
+            scraper that are not found in the sink.
+    """
+    logger = get_run_logger()
+    src_sha_keys = [ i['sha256_surrogate_key'] for i in source_data ]
+    with SqlAlchemyConnector.load("neon-postgres") as database_block:
+        wh_sha_keys = database_block.fetch_all("""
+            SELECT sha256_surrogate_key FROM stg_shopify_apps
+            WHERE sha256_surrogate_key IN :hash_keys
+            """, { "hash_keys": tuple(src_sha_keys) })
+        wh_sha_keys = [ key for single_tuple in wh_sha_keys for key in single_tuple ]
+
+        records_to_write = []
+        for record in source_data:
+            if record['sha256_surrogate_key'] in wh_sha_keys:
+                logger.info(f"No change in app listing for {record['app_name']}")
+                continue
+            else:
+                records_to_write.append(record)
+        
+        print(records_to_write)
+        return(records_to_write)
+
+
+@task(retries=2, retry_delay_seconds=30)
+def load_stg_shopify_apps(data: list[dict]):
+    """Loads data into stg_shopify_apps table.
+    
+    Args:
+        data (list[dict]): List of filtered records from the Shopify app scraper.
+    """
+    with SqlAlchemyConnector.load("neon-postgres") as database_block:
+        logger = get_run_logger()
+        database_block.execute_many("""
+            INSERT INTO stg_shopify_apps (
+                app_name,
+                app_url,
+                ranking,
+                average_rating,
+                total_reviews,
+                is_ad,
+                sha256_surrogate_key
+            ) VALUES (
+                :app_name,
+                :app_url,
+                :ranking,
+                :average_rating,
+                :total_reviews,
+                :is_ad,
+                :sha256_surrogate_key
+            );""", seq_of_parameters=data)
+        logger.info(f"Successfully wrote {len(data)} records into table stg_shopify_apps")
+        return
+
+
+@flow(name="Shopify apps")
+def shopify_apps_flow():
+    url = 'https://apps.shopify.com/search?q=returns+and+exchanges&page=1'
+    data = extract_shopify_apps(url)
+    new_data = compare_sha_keys(data)
+    load_stg_shopify_apps(new_data)
+
+
+def deploy_shopify_apps_flow():
+    shopify_apps_flow.deploy(
+        name="shopify-apps",
+        work_pool_name="docker-pool",
+        image="shopify-apps",
+        cron="0 0,6,12,18 * * *",
+        push=False
+    )
+    return
+
+if __name__ == "__main__":
+    deploy_shopify_apps_flow()
